78992709cc78ab101baa38d7c559e97f4704c77a
musfiqsazal <coder.musfiq@gmail.com>
2024-10-22 20:43:44 -0400

    First Commit: Test

---
+++ Hello-World/Hello-World/Hello-World.cpp
@@ -0,0 +1,46 @@
+// Hello-World.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+//#include<iostream>
+//#include<vector>
+//#include<string>
+//#include<algorithm>
+//#include<stack>
+//#include<unordered_set>
+//#include<cmath>
+//#include<numeric>
+//#include<iterator>
+//#include<set>
+//#include<unordered_map>
+//#include<map>
+//#include<queue>
+//#include<cstring>
+//#include<stdio.h>
+//#include<fstream>
+//#include<iomanip>
+//#include<list>
+//#include<functional>
+//#include<bitset>
+//#include<chrono>
+//#include<climits>
+//#include<assert.h>
+
+
+#include <__msvc_all_public_headers.hpp> 
+using namespace std;
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
